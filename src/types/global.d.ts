a631e0e984dd5fddc6fdb6e5ffbefcc52f248548
Kanyawee <66025997@up.ac.th>
2026-01-05 20:59:21 +0700

    add type declarations for CSS, SCSS, and SASS modules

---
+++ src/types/global.d.ts
@@ -0,0 +1,7 @@
+// Allow importing CSS files as side-effect imports in TypeScript
+// e.g. `import './globals.css'` in Next.js app router
+declare module "*.css";
+
+// Optionally allow SCSS/SASS if used
+declare module "*.scss";
+declare module "*.sass";
